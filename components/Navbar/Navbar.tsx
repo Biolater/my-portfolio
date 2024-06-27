24fe2b4dd7d378a5e05e1657ad8f71513da202bf
Murad <yusifovmurad1@gmail.com>
2024-06-27 18:29:55 +0400

    Drawer update
    Add drawer for displaying hamburger menu

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,27 +1,49 @@
 import Link from "next/link";
-import { Menu } from "lucide-react";
+import { Menu, X } from "lucide-react";
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 const Navbar = () => {
   return (
     <header className="main__header border-b border-[#27272A] flex items-center justify-between p-4">
       <Link href="/" className="main__header__logo">
-        <span className="text-2xl font-semibold">Yusubov</span>
+        <span className="text-2xl font-semibold">yusubov</span>
       </Link>
-      <TooltipProvider delayDuration={300}>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <button className="hamburger__button">
-              <Menu size={30} />
-            </button>
-          </TooltipTrigger>
-          <TooltipContent>Open menu</TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
+      <Drawer direction="right">
+        <TooltipProvider delayDuration={300}>
+          <Tooltip>
+            <DrawerTrigger asChild>
+              <TooltipTrigger asChild>
+                <button className="hamburger__button">
+                  <Menu size={30} />
+                </button>
+              </TooltipTrigger>
+            </DrawerTrigger>
+            <TooltipContent>Open menu</TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        <DrawerContent className="h-screen max-w-[300px] ms-auto">
+          <DrawerHeader className="flex items-center justify-between">
+              <DrawerTitle>Yusubov</DrawerTitle>
+              <DrawerClose>
+                <X className="stroke-white/50 hover:stroke-white/70 active:ring rounded-lg" />
+              </DrawerClose>
+          </DrawerHeader>
+        </DrawerContent>
+      </Drawer>
     </header>
   );
 };
